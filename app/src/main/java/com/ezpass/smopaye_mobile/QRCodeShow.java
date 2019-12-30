0b59aeebef1ed45b86dd44cdb5f744a3f4556eec
Bertin Mounok <bertin.dev@outlook.fr>
2019-12-30 16:04:07 +0100

    Travaux encours sur la génération du QR Code

---
+++ app/src/main/java/com/ezpass/smopaye_mobile/QRCodeShow.java
@@ -0,0 +1,106 @@
+package com.ezpass.smopaye_mobile;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ezpass.smopaye_mobile.Apropos.Apropos;
+import com.ezpass.smopaye_mobile.vuesUtilisateur.ModifierCompte;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class QRCodeShow extends AppCompatActivity {
+
+    private String carte;
+    private ImageView qr_code;
+    private TextView card_number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_show);
+
+        getSupportActionBar().setTitle("QR Code");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        qr_code = findViewById(R.id.qrcode);
+        card_number = findViewById(R.id.card_number);
+
+
+        Intent intent = getIntent();
+        carte = intent.getStringExtra("id_carte");
+        card_number.setText(carte);
+
+
+        if(carte != null && !carte.isEmpty()){
+            try {
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                BitMatrix bitMatrix = multiFormatWriter.encode(carte, BarcodeFormat.QR_CODE, 500, 500);
+                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                qr_code.setImageBitmap(bitmap);
+            } catch (WriterException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+    /*                    GESTION DU MENU DROIT                  */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.apropos) {
+            Intent intent = new Intent(getApplicationContext(), Apropos.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+        if (id == R.id.tuto) {
+            Intent intent = new Intent(getApplicationContext(), TutorielUtilise.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+
+        if(id == R.id.modifierCompte){
+            Intent intent = new Intent(getApplicationContext(), ModifierCompte.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+
+        if (id == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
