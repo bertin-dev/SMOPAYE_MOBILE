4c84a4033ac1cf15b474ae81faaa0169b1931490
Bertin Mounok <bertin.dev@outlook.fr>
2020-01-20 09:34:25 +0100

    passage au domaine d'exploitation

--- app/src/main/java/com/ezpass/smopaye_mobile/ChaineConnexion.java
+++ app/src/main/java/com/ezpass/smopaye_mobile/ChaineConnexion.java
@@ -14,8 +14,8 @@ import javax.crypto.spec.SecretKeySpec;
 public class ChaineConnexion {
 
 
-    //private static final String adresseURLsmopayeServer = "https://cm.secure-ws-api-smp-excecute.smopaye.fr/index.php";
-    private static final String adresseURLsmopayeServer = "https://webservice.domaine.tests.space.smopaye.fr";
+    private static final String adresseURLsmopayeServer = "https://cm.secure-ws-api-smp-excecute.smopaye.fr/index.php";
+    //private static final String adresseURLsmopayeServer = "https://webservice.domaine.tests.space.smopaye.fr";
     private static final String adresseURLGoogleAPI = "https://fcm.googleapis.com/";
     private static final String urlSiteWeb = "https://smopaye.cm/";
     private static final String encrypted_password = "Iyz4BVU2Hlt0cIeIPBlB7Wq15kMDI4NGRmOTNi";
